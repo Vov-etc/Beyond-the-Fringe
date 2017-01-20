d9ca149b6ed46b2bdb31b5e3d085d8e59baefc53
Vov_etc <vladim.kha@ya.ru>
2017-01-20 16:43:12 +0300

    Working now file.cpp

--- Client/src/file.cpp
+++ Client/src/file.cpp
@@ -13,10 +13,19 @@ std::string read_file(const char * filename) {
         sprintf(error_message, "cannot open file %s", filename);
         throw std::runtime_error(error_message);
     }
+    fin.sync();
+	std::string data;
+	getline(fin, data);
     auto buffer = fin.rdbuf();
     int size = buffer->in_avail();
-    std::string data(size + 1, 0);
-    buffer->sgetn(&data[0], size);
+    if (size == 0) {
+        char error_message[200];
+        sprintf(error_message, "File %s is empty", filename);
+        throw std::runtime_error(error_message);
+    }
+	int line_size = data.size();
+	data.resize(size + line_size + 1);
+    buffer->sgetn(&data[line_size], size);
     fin.close();
     return data;
 }
